ec1144158bc205158c2961a720cd709e047e2f0a
aheroglu <ahmet20033134@gmail.com>
2025-06-04 21:38:31 +0300

    Edited environment.ts

--- Day04-Chattr/client/src/environments/environment.ts
+++ Day04-Chattr/client/src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCX-a6EKtqbHP5YXy8XdupwY17B8cDns5I',
-    authDomain: 'chattr-4c24c.firebaseapp.com',
-    projectId: 'chattr-4c24c',
-    storageBucket: 'chattr-4c24c.firebasestorage.app',
-    messagingSenderId: '684869242311',
-    appId: '1:684869242311:web:623af5ed885355085d5de2',
-    measurementId: 'G-D9HWQX6F9X',
-  },
+  firebase: {} // YOUR FIREBASE CONFIGURATION HERE
 };
